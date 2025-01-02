d36f95a55d86d1f3795234aa332282789ccbdf8d
Sizzler_55 <babarsmoke24@gmail.com>
2025-01-02 22:58:14 +0530

    Problem 3 and 4

--- Ts/index.ts
+++ Ts/index.ts
@@ -11,6 +11,42 @@ const NaturalNumberSum = (n: Number): void => {
   }
   console.log(sum);
 };
-NaturalNumber(10);
-NaturalNumberSum(10);
 
+const PerfectNumber = (n) => {
+  let sum = 1;
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      sum += i;
+      if (n / i != i) {
+        sum += n / i;
+      }
+    }
+  }
+  return sum == n;
+};
+
+const AllNum = (start, end) => {
+  for (let i = start; i <= end; i++) {
+    if (PerfectNumber(i)) {
+      console.log("PN");
+      console.log(i);
+    }
+  }
+};
+
+const PrimeNumber = (n) => {
+  let count = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      count++;
+      if (n / i != i) {
+        count++;
+      }
+    }
+  }
+  count == 2 ? console.log("Rn") : console.log("Nrn");
+};
+// NaturalNumberSum(10);
+// AllNum(6, 500);
+//
+PrimeNumber(9);
