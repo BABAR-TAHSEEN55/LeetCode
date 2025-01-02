1a08c9e541939360d812ff3b59674b000241ac31
Sizzler_55 <babarsmoke24@gmail.com>
2025-01-02 22:58:02 +0530

    Problem 3 & 4

--- Cpp/Problem5.cpp
+++ Cpp/Problem5.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace  std; 
-    
 
 
-void NaturalNumbers(int n) {
+void naturalnumbers(int n) {
   for(int i =1;i<=10;i++){
 cout<<i<<endl;
   }
 }
 
-void NaturalNumbersSum(int n) {
+void naturalnumberssum(int n) {
   int sum = 0;
   for(int i = 0;i<=n;i++){
 
@@ -18,7 +17,58 @@ sum+=i;
   cout<<sum;
 }
 
+bool perfectnumber(int n ) {
+  int sum = 1;
+for(int i = 2;i*i<=n;i++){
+    if(n%i==0){
+      sum+=i;
+ 
+    if(n/i!=i){
+      sum+=(n/i);
+    }
+//If you dont nest it then it'll run independently if even if i is not the divisior
+    }
+  }
+  return(sum==n)?true:false;
+
+}
+
+
+// void AllNum(int start , int end ){
+//   for(int i = start;i<=end;i++){
+//     if(perfectnumber(i)){
+//       cout<<"pn"<<endl;
+//       cout<<i;
+//     }
+//   }
+// }
+
+void PrimeNumber(int n){
+  int count =0;
+  for(int i = 1;i*i<=n;i++){
+    if(n%i==0){
+        count++;
+      if((n/i)!=i){
+        count++;
+      }
+    }
+  }
+  count==2?cout<<"PM":cout<<"NPM";
+}
+
+// void AllNum(int start , int end ){
+//   for(int i = start;i<=end;i++){
+//     if(PrimeNumber(i)){
+//       cout<<"pn"<<endl;
+//       cout<<i;
+//     }
+//   }
+// }
+
 int main(){
-  // NaturalNumbers(10);
-  // NaturalNumbersSum(10);
+  // naturalnumbers(10);
+  // naturalnumberssum(10);
+    // perfectnumber(28);
+  // AllNum(6,500);
+  PrimeNumber(9);
 }
