c42bea0cbf0ae55ccde75a407fd0ed36d0c9330d
Sizzler_55 <babarsmoke24@gmail.com>
2025-01-04 22:33:09 +0530

    7 & 8 Problem Solved

--- Ts/index.ts
+++ Ts/index.ts
@@ -34,7 +34,8 @@ const AllNum = (start, end) => {
   }
 };
 
-const PrimeNumber = (n) => {
+const PrimeNumber = (n): boolean => {
+  if (n < 2) return;
   let count = 0;
   for (let i = 1; i * i <= n; i++) {
     if (n % i == 0) {
@@ -44,9 +45,45 @@ const PrimeNumber = (n) => {
       }
     }
   }
-  count == 2 ? console.log("Rn") : console.log("Nrn");
+  return count === 2;
 };
+const RunAll = (start, end) => {
+  for (let i = start; i <= end; i++) {
+    if (PrimeNumber(i)) {
+      console.log(i);
+    }
+  }
+};
+const PrevPrime = (n) => {
+  for (let i = n - 1; i >= 2; i--) {
+    if (PrimeNumber(i)) {
+      console.log(i);
+      return;
+    }
+  }
+};
+const Gcd = (a: number, b: number): number => {
+  while (a > 0 && b > 0) {
+    a > b ? (a %= b) : (b %= 10);
+  }
+  return a === 0 ? b : a;
+};
+
+const SumofDigits = (n: number): number => {
+  let sum = 0;
+  while (n > 0) {
+    let digit = n % 10;
+    n = Math.floor(n / 10);
+    sum += digit;
+  }
+  return sum;
+};
+// console.log(4 / 2);
 // NaturalNumberSum(10);
 // AllNum(6, 500);
 //
-PrimeNumber(9);
+// PrimeNumber(9);
+// RunAll(2, 19);
+// PrevPrime(10);
+// console.log(Gcd(50, 100));
+console.log(SumofDigits(123));
