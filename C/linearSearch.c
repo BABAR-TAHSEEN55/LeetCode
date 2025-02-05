e8d6f7999544d296198e631a2638ccb18bec0b6e
Babar <babarsmoke24@gmail.com>
2025-02-05 19:35:37 +0530

    Linear Search Implemented

---
+++ C/linearSearch.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void LinearSearch(int n , int arr[]){
+  int element;
+  printf("Enter Element Value");
+  scanf("%d",&element);
+  for(int i = 0;i<=n;i++){
+    if(arr[i]==element){
+      printf("element found at index : %d",i);
+      break;
+    }
+  }
+}
+int main(){
+  int arr[]= {1,2,3,4,5};
+LinearSearch(5,arr);
+}
