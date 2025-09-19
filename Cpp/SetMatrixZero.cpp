bc7a1aeacfdc491f5c8c96d25a3694c7568cbf3d
Mohammed Babar Tahseen <babarsmoke24@gmail.com>
2025-09-19 22:19:49 +0530

    Implement better solution for setting matrix zeroes
    Added a better solution to set matrix zeroes using row and column tracking.

--- Cpp/SetMatrixZero.cpp
+++ Cpp/SetMatrixZero.cpp
@@ -28,3 +28,24 @@ void MarkColumnZero(j){
 int main(){
 SetZero(arr,4,3);
 }
+
+void BetterSolution ( int arr[] , int m , int n ) {
+ int col[m]={0};
+   int Row[n]={0};
+  
+  for( int i =0 ;i<m;i++){
+  for( int j=0;j<m;j++){
+    if(arr[i][j]==0){
+      Row[i]= 1;
+      col[j]=2;
+    }
+  }
+  }
+  for( int i =0 ;i<m;i++){
+  for( int j=0;j<m;j++){
+    if(Row[i] ||col [j] ){
+      arr[i][j]=0;
+    }
+  }
+  }
+}
